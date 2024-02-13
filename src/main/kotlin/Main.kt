2f21b09e39bb2555aeb919b21904ddde5975ad74
Alexey Fedechkin <alex@centralhardware.me>
2024-02-13 22:32:09 +0100

    send row json

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,20 @@
 import dev.inmo.tgbotapi.extensions.api.answers.answer
 import dev.inmo.tgbotapi.extensions.api.bot.setMyCommands
+import dev.inmo.tgbotapi.extensions.api.send.media.sendDocument
 import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
 import dev.inmo.tgbotapi.extensions.behaviour_builder.telegramBotWithBehaviourAndLongPolling
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onAnyInlineQuery
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommandWithArgs
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.text
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.InlineKeyboardMarkup
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.dataButton
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.inlineKeyboard
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.replyKeyboard
+import dev.inmo.tgbotapi.requests.abstracts.InputFile
 import dev.inmo.tgbotapi.types.BotCommand
 import dev.inmo.tgbotapi.types.InlineQueries.InlineQueryResult.InlineQueryResultArticle
 import dev.inmo.tgbotapi.types.InlineQueries.InputMessageContent.InputTextMessageContent
+import dev.inmo.tgbotapi.utils.row
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import org.berndpruenster.netlayer.tor.NativeTor
@@ -17,19 +24,22 @@ import org.json.JSONObject
 import java.io.File
 import java.math.BigInteger
 
-fun getData(id: BigInteger): JSONObject {
-    return khttp.get(" https://info.midpass.ru/api/request/${id}").jsonObject
+fun getData(id: BigInteger): Pair<JSONObject, String> {
+    val res = khttp.get(" https://info.midpass.ru/api/request/${id}")
+    return Pair(res.jsonObject, res.text)
 }
 
-fun getStatus(id: BigInteger): String {
-    var res = getData(id)
-    return """
-        Статус паспорта: ${res.getJSONObject("passportStatus").get("name")}
-        Статус заявки: ${res.getJSONObject("internalStatus").get("name")}
-        Дата обновления: ${res.get("receptionDate")}
+fun getStatus(id: BigInteger): Pair<File, String> {
+    var data = getData(id)
+    val res = """
+        Статус паспорта: ${data.first.getJSONObject("passportStatus").get("name")}
+        Статус заявки: ${data.first.getJSONObject("internalStatus").get("name")}
+        Дата обновления: ${data.first.get("receptionDate")}
     """.trimIndent()
+    val file = File("$id.json")
+    file.writeText(data.second)
+    return Pair(file, res)
 }
-
 
 
 suspend fun main() {
@@ -39,47 +49,54 @@ suspend fun main() {
     TorSocket("info.midpass.ru", 443, streamId = "FOO" /*this one is optional*/) //clear web
 
     telegramBotWithBehaviourAndLongPolling(System.getenv("BOT_TOKEN"),
-        CoroutineScope(Dispatchers.IO,),
-        defaultExceptionsHandler = { println(it) }){
-            setMyCommands(
-                BotCommand("q", "узнать статус заявления. Использование /q <номер заявки>")
-            )
-            onCommandWithArgs("q") {message, args ->
-                println(message.text)
+        CoroutineScope(Dispatchers.IO),
+        defaultExceptionsHandler = { println(it) }) {
+        setMyCommands(
+            BotCommand("q", "узнать статус заявления. Использование /q <номер заявки>")
+        )
+        onCommandWithArgs("q") { message, args ->
+            println(message.text)
 
-                if (args.size != 1){
-                    sendTextMessage(message.chat, "Введите номер заявки")
-                    return@onCommandWithArgs
-                }
-
-                val id = args.first().toBigIntegerOrNull()
-
-                if (id == null){
-                    sendTextMessage(message.chat, "Введите номер заявки")
-                    return@onCommandWithArgs
-                }
-
-                sendTextMessage(message.chat, getStatus(id))
+            if (args.size != 1) {
+                sendTextMessage(message.chat, "Введите номер заявки")
+                return@onCommandWithArgs
             }
-            onAnyInlineQuery {
-                val id = it.query.toBigIntegerOrNull()
 
-                var res = ""
-                if (id == null){
-                    res = "Введите номер заявки"
-                    return@onAnyInlineQuery
-                } else {
-                    res = getStatus(id)
-                }
+            val id = args.first().toBigIntegerOrNull()
 
-                answer(it,
-                    listOf(InlineQueryResultArticle(
+            if (id == null) {
+                sendTextMessage(message.chat, "Введите номер заявки")
+                return@onCommandWithArgs
+            }
+
+            val status = getStatus(id)
+            sendTextMessage(message.chat, status.second)
+            sendDocument(message.chat, InputFile.fromFile(status.first))
+            status.first.delete()
+        }
+        onAnyInlineQuery {
+            println(it.query)
+            val id = it.query.toBigIntegerOrNull()
+
+            var res = ""
+            if (id == null) {
+                res = "Введите номер заявки"
+                return@onAnyInlineQuery
+            } else {
+                res = getStatus(id).second
+            }
+
+            answer(
+                it,
+                listOf(
+                    InlineQueryResultArticle(
                         it.query,
                         it.query,
                         InputTextMessageContent(res)
-                    ))
+                    )
                 )
-            }
-        }.second.join()
+            )
+        }
+    }.second.join()
 
 }
